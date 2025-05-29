1bb663652e46f9b1e05a186050bfab622cbf7b49
Hades <gbalyukw@gmail.com>
2025-05-29 02:00:03 -0400

    Adding function for answer identification by token

--- ads-test-master/data-science/chatbot/main.py
+++ ads-test-master/data-science/chatbot/main.py
@@ -3,6 +3,7 @@ from transformers import (
     AutoModelForQuestionAnswering,
 )
 from datasets import load_dataset
+
 model_name = "distilbert-base-uncased"
 model_dir = "./model_output"
 
@@ -10,6 +11,49 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForQuestionAnswering.from_pretrained(model_name)
 
 squad = load_dataset("squad")
+
+
+def process_answer_tokens(record):
+    inputs = tokenizer(
+        record["question"],
+        record["context"],
+        truncation="only_second",
+        padding="max_length",
+        max_length=384,
+        return_offsets_mapping=True,
+        return_token_type_ids=True
+    )
+
+    answer = record["answers"]["text"][0]
+    answer_start_index = record["answers"]["answer_start"][0]
+    answer_end_index = answer_start_index + len(answer)
+
+    offsets = inputs["offset_mapping"]
+    token_types = inputs["token_type_ids"]
+
+    start_token = end_token = 0
+    for i in range(len(offsets)):
+        start, end = offsets[i]
+        type_id = token_types[i]
+
+        if type_id != 1:
+            continue
+
+        if start <= answer_start_index < end:
+            start_token = i
+        if start < answer_end_index <= end:
+            end_token = i
+            break
+
+    inputs["start_positions"] = start_token
+    inputs["end_positions"] = end_token
+    inputs.pop("offset_mapping")
+    return inputs
+
+
+training = squad["train"].select(range(200)).map(process_answer_tokens)
+validation = squad["validation"].select(range(50)).map(process_answer_tokens)
+
 
 def describe_model():
     """
